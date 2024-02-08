7755a000a697450b5003ba87ecdeba0d8306a804
xMisael117052x <157148695+xMisael117052x@users.noreply.github.com>
2024-02-08 10:06:11 -0600

    Avance de menu

--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -17,6 +17,37 @@ void Sistema::setProveedor(Proveedor _proveedor)
 
 void Sistema::optionCliente()
 {
+	// Iniciar variables
+	string nombre;
+	int puntos;
+	int telefono;
+	string correo;
+
+	//Pedir datos
+	cout << "Ingresa el nombre del cliente: " << endl;
+	cin >> nombre;
+	cin.ignore();
+
+	cout << "Introduce los puntos de " << nombre << ":" << endl;
+	cin >> puntos;
+
+	cout << "Introduce el numero de telefono de " << nombre << ":" << endl;
+	cin >> telefono;
+
+	cout << "Introduce el correo electronico de " << nombre << ":" << endl;
+	cin >> correo;
+	cin.ignore();
+
+	// Crear objeto cliente
+	Cliente cliente = Cliente();
+
+	// Introducir datos
+	cliente.setNombre(nombre);
+	cliente.setPuntos(puntos);
+	cliente.setTelefono(telefono);
+	cliente.setCorreo(correo);
+
+	setCliente(cliente);
 }
 
 void Sistema::optionProveedor()
@@ -29,8 +60,9 @@ void Sistema::optionProducto()
 
 Sistema::Sistema()
 {
-
 	cout << "Bienveindo al sistema de oxxo" << endl;
+
+	// Menu de opciones
 	bool continuar = true;
 	while (continuar) {
 		int opcion = 0;
@@ -43,14 +75,24 @@ Sistema::Sistema()
 		cout << "Seleccione una opcion del menu" << endl;
 		cin >> opcion;
 
+		// Ingresar a la seleccion
 		switch (opcion) {
 		case 1:
+			optionCliente();
 			break;
 		case 2:
 			break;
 		case 3:
 			break;
 		default:
+		}
+
+		// Repetir programa
+		int opcionRepetir = 0;
+		cout << "Quieres usar otra opcion? 1 - Si, 2 - No" << endl;
+		cin >> opcionRepetir;
+		if (opcionRepetir == 2) {
+			continuar = false;
 		}
 	}
 }
